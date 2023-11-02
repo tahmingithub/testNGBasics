6944227eaebf436dce7781cf75d5ba20a8948c48
tahmingithub <tahminnka@gmail.com>
2023-11-02 12:08:02 -0400

    mycodes

---
+++ src/test/java/class01/testNGannotations.java
@@ -0,0 +1,30 @@
+package class01;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class testNGannotations {  // name of the function
+
+    @BeforeMethod  // before method uses before @test
+    public void beforeMethod() {
+        System.out.println("i am before method");
+    }
+
+    @Test(groups="regression")
+    public void aTestCase() {
+        System.out.println("i am the first test case");
+    }
+
+    @Test
+    public void bTestCase() {
+        System.out.println("i am the second test case");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("i am after method");
+    }
+
+
+}
---
+++ src/test/java/class02/listener.java
@@ -0,0 +1,21 @@
+package class02;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class listener  implements ITestListener {     // listener is what we name it
+
+         @Override
+         public void  onTestSuccess(ITestResult result){
+             System.out.println("The test case has passed"+result.getName());
+         }
+
+         @Override
+         public void onTestFailure(ITestResult result){
+             System.out.println("the test case failed"+result.getName());
+         }
+     }
+
+
+
+
